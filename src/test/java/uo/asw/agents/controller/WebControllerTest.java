29a1a6a1f6f32161fe6a23e62719f99cc7b4eac7
Pelayo Díaz Soto <UO251000@uniovi.es>
2018-02-14 20:35:59 +0100

    Tests adaptados a nuevos métodos.

--- src/test/java/uo/asw/agents/controller/WebControllerTest.java
+++ src/test/java/uo/asw/agents/controller/WebControllerTest.java
@@ -99,7 +99,7 @@ public class WebControllerTest {
    @Test
    public void changePasswordTest1() throws Exception {
    	
-	   Loader c = loaderDAO.getAgents("juan", "1234");
+	   Loader c = loaderDAO.getAgents("juan", "1234", "admin");
 
 	   //Cambio de contraseña
        mockMvc.perform(post("/changeInfo")
@@ -123,7 +123,7 @@ public class WebControllerTest {
    //Contraseña incorrecta
    public void changePasswordTest2() throws Exception {
    	
-	   Loader c = loaderDAO.getAgents("juan", "1234");
+	   Loader c = loaderDAO.getAgents("juan", "1234", "admin");
 
        mockMvc.perform(post("/changeInfo")
     	.param("password", "password")
@@ -136,7 +136,7 @@ public class WebControllerTest {
    @Test
    public void changeEmailTest1() throws Exception {
    	
-	   Loader c = loaderDAO.getAgents("juan", "1234");
+	   Loader c = loaderDAO.getAgents("juan", "1234", "admin");
 
 	   //Cambio de email
        mockMvc.perform(post("/changeEmail")
